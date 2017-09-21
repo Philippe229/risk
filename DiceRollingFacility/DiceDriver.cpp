c535f4638f9805ecbf1f47ba492289a32567bd55
Philippe Li <philo1196@gmail.com>
2017-09-21 08:49:13 -0400

    changed loop count

--- DiceRollingFacility/DiceDriver.cpp
+++ DiceRollingFacility/DiceDriver.cpp
@@ -25,14 +25,14 @@ int main() {
 	cout << "\ndiceRollingFacility1 history:" << endl;
 	diceRollingFacility1.printDiceRollHistory();
 
-	int loopCount = 5000;
+	int loopCount = 1000;
 
 	while (loopCount > 0) {
 		diceRollingFacility2.throwDie(3);
 		loopCount--;
 	}
 
-	cout << "\ndiceRollingFacility2 history after 5000 dice throws:" << endl;
+	cout << "\ndiceRollingFacility2 history after 3000 dice throws:" << endl;
 	diceRollingFacility2.printDiceRollHistory();
 
 	return 0;
