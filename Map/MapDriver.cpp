4cde582b91a8c70a4a6fb2b16e033486a73a0520
benrs <benjaminbarault@gmail.com>
2017-09-25 11:44:49 -0400

    Fixed map driver

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -11,20 +11,20 @@ int main() {
 	Player *roosevelt = new Player("Teddy Roosevelt");
 	Player *gandhi = new Player("Gandhi");
 
-	vector<Country> countries;
-	countries.push_back(Country("USA", "America", *roosevelt, 10));
-	countries.push_back(Country("India", "Asia", *gandhi, 20));
-	countries.push_back(Country("England", "Europe", *roosevelt, 5));
-	countries.push_back(Country("China", "Asia", *gandhi, 20));
+	vector<Country*> countries;
+	countries.push_back(new Country("USA", "America", *roosevelt, 10));
+	countries.push_back(new Country("India", "Asia", *gandhi, 20));
+	countries.push_back(new Country("England", "Europe", *roosevelt, 5));
+	countries.push_back(new Country("China", "Asia", *gandhi, 20));
 
-	countries[0].addBorderingCountry(&countries[1]);
-	countries[1].addBorderingCountry(&countries[0]);
-	countries[1].addBorderingCountry(&countries[2]);
-	countries[2].addBorderingCountry(&countries[1]);
-	countries[2].addBorderingCountry(&countries[3]);
-	countries[3].addBorderingCountry(&countries[2]);
-	countries[3].addBorderingCountry(&countries[0]);
-	countries[0].addBorderingCountry(&countries[3]);
+	countries[0]->addBorderingCountry(countries[1]);
+	countries[1]->addBorderingCountry(countries[0]);
+	countries[1]->addBorderingCountry(countries[2]);
+	countries[2]->addBorderingCountry(countries[1]);
+	countries[2]->addBorderingCountry(countries[3]);
+	countries[3]->addBorderingCountry(countries[2]);
+	countries[3]->addBorderingCountry(countries[0]);
+	countries[0]->addBorderingCountry(countries[3]);
 
 	Map *world = new Map(countries);
 	world->getCountries();
