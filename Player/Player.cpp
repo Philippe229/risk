f7bff29c07c4a1c0ea8f9e42a004baf1683e82b1
pbgnz <pabgonza1995@gmail.com>
2017-09-28 00:16:18 -0400

    #4 use regular for-loop to remove countries

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -58,11 +58,10 @@ void Player::addCountry(Country* c) {
 }
 
 void Player::removeCountry(Country* c) {
-	for (auto& country : countries) {
-		if (country == c) {
+	for (size_t i = 0; i < countries.size(); i++) {
+		if (countries[i] == c) {
 			//TODO: update new country's owner
-			countries.erase(
-					countries.begin() + (&country - &*(countries.begin())));
+			countries.erase(countries.begin() + i);
 			break;
 		}
 	}
