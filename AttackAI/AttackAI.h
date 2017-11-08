31f1008008748f83c40ae84783b60b5c1a4f8a4e
Dylan Fernandes <dylangfernandes@gmail.com>
2017-11-08 17:49:52 -0500

    made attack more generic

--- AttackAI/AttackAI.h
+++ AttackAI/AttackAI.h
@@ -8,23 +8,9 @@ using namespace std;
 
 class AttackAI {
 
-private:
-	void updateAttack();
-	// int numberOfCountriesConquered;
-	// void attackProcedure();
-	bool isContained(Country*,vector<Country*>);
-	bool validateNumericInput(int&,int,int);
-	// bool validateSpecNumericInput(int&,vector<int>);
-    bool attack(Country*, Country*);
-    int id;
-    Player* pl;
-    Deck* deck; // get gameloop's deck
-    vector<Country*> countries;
-    vector<Country*> attackBase;
-	vector<Country*> attackPossibilities;
 
 public:
-	void attackInitalization(Player*, Deck* deck);
+	static bool attack(Country*, Country*, int);
 
 };
 
