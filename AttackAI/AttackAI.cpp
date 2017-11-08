4031d0d4cba078308e6ea798f8105bed2b5a9589
Dylan Fernandes <dylangfernandes@gmail.com>
2017-11-07 23:20:45 -0500

    initial setup for attackAI

---
+++ AttackAI/AttackAI.cpp
@@ -0,0 +1,7 @@
+#include "AttackAI.h"
+
+void AttackAI::attackInitalization(Player*, Deck* deck) {
+    while(true) {
+        
+    }
+}
---
+++ AttackAI/AttackAI.h
@@ -0,0 +1,31 @@
+#ifndef ATTACKAI_H_
+#define ATTACKAI_H_
+
+#include "../Player/Player.h"
+#include "../CardComponents/Deck.h"
+
+using namespace std;
+
+class Attack {
+
+private:
+	// void updateAttack();
+	// int numberOfCountriesConquered;
+	// void attackProcedure();
+	// bool isContained(Country*,vector<Country*>);
+	// bool validateNumericInput(int&,int,int);
+	// bool validateSpecNumericInput(int&,vector<int>);
+    // bool attack(Country*, Country*);
+    int id;
+    Player* pl;
+    Deck* deck; // get gameloop's deck
+    vector<Country*> countries;
+    vector<Country*> attackBase;
+	vector<Country*> attackPossibilities;
+
+public:
+	void attackInitalization(Player*, Deck* deck);
+
+};
+
+#endif /* ATTACKAI_H_ */
