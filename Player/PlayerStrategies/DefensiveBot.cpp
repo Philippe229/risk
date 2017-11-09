e20357db77043e95ce103db7b3499d2a3cb003e9
pbgnz <pabgonza1995@gmail.com>
2017-11-09 01:50:37 -0500

    changed defense bot fortify

--- Player/PlayerStrategies/DefensiveBot.cpp
+++ Player/PlayerStrategies/DefensiveBot.cpp
@@ -20,18 +20,18 @@ void DefensiveBot::reinforce(Map* currMap, Deck* currDeck) {
     Reinforcement::staticBonusArmies = Reinforcement::getTotalBonusArmies(this, currMap->getContinents());
     int incrementBy;
 
-    // Sort the player's countries in ascending according to the number of armies
+    // Sort countries in ascending according to the number of armies
     vector<Country*> myCountries = Player::getCountries();
     std::sort( myCountries.begin(), myCountries.end(), [ ](Country*& lhs, Country*& rhs)
     {
        return lhs->getArmies() < rhs->getArmies();
     });
 
-
     // take the difference between the (i+1)th country armies and the (i)th country armies
     // and assign that difference to every country that is less or equal to (i)
     for (int i = 0; i < myCountries.size(); i++) {
 
+    	cout << myCountries[i]->getArmies() << endl;
         incrementBy = myCountries[i+1]->getArmies() - myCountries[i]->getArmies();
 
         if ((incrementBy*(i+1)) <= Reinforcement::staticBonusArmies) {
@@ -44,6 +44,7 @@ void DefensiveBot::reinforce(Map* currMap, Deck* currDeck) {
         	}
         	break; // all the bonus armies allocated
         }
+
     }
 }
 
@@ -52,18 +53,26 @@ void DefensiveBot::attack(Map* currMap, Deck* currDeck) {
 }
 
 void DefensiveBot::fortify(Map* currMap, Deck* currDeck) {
-	int incrementBy;
-
-	// Sort the player's countries in ascending of number of armies
     vector<Country*> myCountries = Player::getCountries();
-    std::sort( myCountries.begin(), myCountries.end(), [ ](Country*& lhs, Country*& rhs)
-    {
-       return lhs->getArmies() < rhs->getArmies();
+    Country* weakestCountry = NULL;
+
+    // Get the weakest country (least armies)
+    for (int i = 0; i < myCountries.size(); i++) {
+        if (weakestCountry == NULL || myCountries[i]->getArmies() < weakestCountry->getArmies()) {
+        	weakestCountry = myCountries[i];
+        }
+    }
+
+    // Sort countries by number of armies
+    sort(myCountries.begin(), myCountries.end(), [](Country* lhs,Country* rhs) {
+        return lhs->getArmies() < rhs->getArmies();
     });
 
-    int size = myCountries.size();
-
-    incrementBy = (myCountries[size - 1]->getArmies() - myCountries[0]->getArmies()) / 2;
-    Fortification::fortify(this, myCountries[size - 1], myCountries[0], incrementBy);
-
+    // Try to fortify the country with the most armies with the weakest country
+    for (int i = myCountries.size() - 1; i >= 0; i--) {
+        if (weakestCountry != myCountries[i] && Fortification::verifyTargetCountry(this, weakestCountry, myCountries[i])) {
+            Fortification::fortify(this, myCountries[i], weakestCountry, myCountries[i]->getArmies() - 1);
+            break;
+        }
+    }
 }
